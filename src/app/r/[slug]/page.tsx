e792ba48458e54f65dec45152382bd3b12110859
Patryk Polanski <pat.polanski@gmail.com>
2024-02-15 18:07:23 +0000

    start working on the subreddit page, fetch subreddit data from db with a server action

---
+++ src/app/r/[slug]/page.tsx
@@ -0,0 +1,42 @@
+import { notFound } from "next/navigation";
+
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
+
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default async function SubredditPage({ params }: PageProps) {
+  const { slug } = params;
+
+  const session = await getAuthSession();
+
+  const subreddit = await db.subreddit.findFirst({
+    where: {
+      name: slug,
+    },
+    include: {
+      posts: {
+        include: {
+          author: true,
+          votes: true,
+          comments: true,
+          subreddit: true,
+        },
+
+        take: INFINITE_SCROLLING_PAGINATION_RESULTS,
+      },
+    },
+  });
+
+  // throw 404 if there is no data
+  if (!subreddit) return notFound();
+
+  return (
+    <h1 className="font-bold text-3xl md:text-4xl h-14">r/{subreddit.name}</h1>
+  );
+}
