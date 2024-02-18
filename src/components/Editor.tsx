1a4012054acd0262896777a1da2004a642c1d9c1
Patryk Polanski <pat.polanski@gmail.com>
2024-02-18 12:38:54 +0000

    Editor: initialize editor with all plugins, create an api route handle to fetch link data

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -3,8 +3,10 @@
 import { useForm } from "react-hook-form";
 import TextAreaAutosize from "react-textarea-autosize";
 import { zodResolver } from "@hookform/resolvers/zod";
+import type EditorJS from "@editorjs/editorjs";
 
 import { PostCreationRequest, PostValidator } from "@/lib/validators/post";
+import { useCallback, useRef } from "react";
 
 interface EditorProps {
   subredditId: string;
@@ -23,6 +25,41 @@ export default function Editor({ subredditId }: EditorProps) {
       content: null,
     },
   });
+
+  const ref = useRef<EditorJS>();
+
+  const initializeEditor = useCallback(async () => {
+    const EditorJS = (await import("@editorjs/editorjs")).default;
+    const Header = (await import("@editorjs/header")).default;
+    const Embed = (await import("@editorjs/embed")).default;
+    const Table = (await import("@editorjs/table")).default;
+    const List = (await import("@editorjs/list")).default;
+    const Code = (await import("@editorjs/code")).default;
+    const LinkTool = (await import("@editorjs/link")).default;
+    const InlineCode = (await import("@editorjs/inline-code")).default;
+    const ImageTool = (await import("@editorjs/image")).default;
+
+    if (!ref.current) {
+      const editor = new EditorJS({
+        holder: "editor",
+        onReady() {
+          ref.current = editor;
+        },
+        placeholder: "Type here to write your post...",
+        inlineToolbar: true,
+        data: { blocks: [] },
+        tools: {
+          header: Header,
+          linkTool: {
+            class: LinkTool,
+            config: {
+              endpoint: "/api/link",
+            },
+          },
+        },
+      });
+    }
+  }, []);
 
   return (
     <div className="w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200">
