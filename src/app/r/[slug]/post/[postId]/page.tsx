3bb10b3086dd39024cc29989c44fecab9046a37a
Patryk Polanski <pat.polanski@gmail.com>
2024-02-22 17:22:40 +0000

    work on single post page, create PostVoteServer, stream less important data (post votes)

---
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -0,0 +1,98 @@
+import { notFound } from "next/navigation";
+import { redis } from "@/lib/reddis";
+import { db } from "@/lib/db";
+import { Post, User, Vote } from "@prisma/client";
+
+import { CachedPost } from "@/types/redis";
+
+import { Suspense } from "react";
+import { ArrowBigDown, ArrowBigUp, Loader2 } from "lucide-react";
+
+import { buttonVariants } from "@/components/ui/Button";
+import PostVoteServer from "@/components/post-vote/PostVoteServer";
+import { formatTimeToNow } from "@/lib/utils";
+import EditorOutput from "@/components/EditorOutput";
+
+interface PageProps {
+  params: {
+    postId: string;
+  };
+}
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
+export default async function PostPage({ params }: PageProps) {
+  const cachedPost = (await redis.hgetall(
+    `post:${params.postId}`
+  )) as CachedPost;
+
+  let post: (Post & { votes: Vote[]; author: User }) | null = null;
+
+  if (!cachedPost) {
+    post = await db.post.findFirst({
+      where: {
+        id: params.postId,
+      },
+      include: {
+        votes: true,
+        author: true,
+      },
+    });
+  }
+
+  if (!post && !cachedPost) return notFound();
+
+  return (
+    <div>
+      <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between">
+        <Suspense fallback={<PostVoteShell />}>
+          {/* @ts-expect-error */}
+          <PostVoteServer
+            postId={post?.id ?? cachedPost.id}
+            getData={async () => {
+              return await db.post.findUnique({
+                where: {
+                  id: params.postId,
+                },
+                include: {
+                  votes: true,
+                },
+              });
+            }}
+          />
+        </Suspense>
+
+        <div className="w-full bg-white p-4 rounded-sm">
+          <p className="max-h-40 mt-1 truncate text-xs text-gray-500">
+            Posted by u/{post?.author.username ?? cachedPost.authorUsername}{" "}
+            {formatTimeToNow(new Date(post?.createdAt ?? cachedPost.createdat))}
+          </p>
+          <h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">
+            {post?.title ?? cachedPost.title}
+          </h1>
+
+          <EditorOutput content={post?.content ?? cachedPost.content} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function PostVoteShell() {
+  return (
+    <div className="flex items-center flex-col pr-6 wi20">
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigUp className="h-5 w-5 text-zinc-700" />
+      </div>
+
+      <div className="text-center py-2 font-medium text-sm text-zinc-900">
+        <Loader2 className="h-3 w-3 animate-spin" />
+      </div>
+
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigDown className="h-5 w-5 text-zinc-700" />
+      </div>
+    </div>
+  );
+}
