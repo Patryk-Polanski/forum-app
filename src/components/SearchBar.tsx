cf4e42cc950c3671308ff26cac5264716647471f
Patryk Polanski <pat.polanski@gmail.com>
2024-02-23 23:16:34 +0000

    create an api route handler for search quaries, add Command shadcn component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,14 +1,25 @@
 "use client";
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { useRouter } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-import { Command, CommandInput } from "./ui/Command";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandList,
+} from "./ui/Command";
 import { Prisma, Subreddit } from "@prisma/client";
+import { CommandItem } from "cmdk";
+import { Users } from "lucide-react";
+import debounce from "lodash.debounce";
 
 export default function SearchBar() {
   const [input, setInput] = useState<string>("");
+  const router = useRouter();
 
   // we can call refetch() when needed
   const {
@@ -22,20 +33,54 @@ export default function SearchBar() {
     queryFn: async () => {
       if (!input) return [];
       const { data } = await axios.get(`/api/search?q=${input}`);
-      return data as Subreddit & {
+      return data as (Subreddit & {
         _count: Prisma.SubredditCountOutputType;
-      };
+      })[];
     },
   });
+
+  const request = debounce(() => {
+    refetch();
+  }, 500);
+
+  const debounceRequest = useCallback(() => {
+    request();
+  }, []);
 
   return (
     <Command className="relative rounded-lg border max-w-lg z-50 overflow-visible">
       <CommandInput
         value={input}
-        onValueChange={(text) => setInput(text)}
+        onValueChange={(text) => {
+          setInput(text);
+          debounceRequest();
+        }}
         className="outline-none border-none focus:border-none focus:outline-none ring-0"
         placeholder="Search communities..."
       />
+      {input.length > 0 ? (
+        <CommandList className="absolute bg-white top-full inset-x-0 shadow rounded-b-md">
+          {isFetched && <CommandEmpty>No results found.</CommandEmpty>}
+          {(queryResults?.length ?? 0) > 0 ? (
+            <CommandGroup heading="Communities">
+              {queryResults?.map((subreddit) => (
+                <CommandItem
+                  key={subreddit.id}
+                  value={subreddit.name}
+                  className="flex gap-2 items-center px-2 py-1"
+                  onSelect={(e) => {
+                    router.push(`/r/${e}`);
+                    router.refresh();
+                  }}
+                >
+                  <Users className="mr-2 h-4 w-4" />
+                  <a href={`/r/${subreddit.name}`}>r/{subreddit.name}</a>
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          ) : null}
+        </CommandList>
+      ) : null}
     </Command>
   );
 }
